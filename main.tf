26b73b481dc653d4a68adef5c3a35c91b9e731ad
deepak-spundan03 <deepakmds78@gmail.com>
2020-04-15 07:30:02 -0400

    bulcky commit with nginx provising

--- main.tf
+++ main.tf
@@ -158,28 +158,47 @@ resource "aws_security_group_rule" "allow-outbound-http" {
 
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "aws_key_pair" "auth" {
+  key_name = "demo-terraform"
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+   
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "hello-nginx"
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = var.aws_amis.var.aws_region
+
+  # The name of our SSH keypair we created above.
+  key_name = demo-terraform
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
   }
 }
-
